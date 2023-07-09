7e39d4b374a158a52e67b2c8438d9ed643f521e0
AMIR MUSTAFA <amirengg15@gmail.com>
2023-07-09 16:45:16 +0530

    docs: updating exp table

---
+++ service/data/experiencetable.js
@@ -0,0 +1,33 @@
+export const data = {
+    ey: [
+      { area: 'Frontend', technology: 'React JS, Javascript' },
+      { area: 'Backend', technology: 'Nest JS, Node JS' },
+      { area: 'Store', technology: 'MobX' },
+      { area: 'Cloud', technology: 'Azure' },
+      { area: 'Cloud Services', technology: 'Azure Form Recognizer' },
+      { area: 'Version Control System', technology: 'Azure Repos' },
+      { area: 'CI/CD', technology: 'Local, Dev, UAT, Production' },
+      { area: 'Tracking System', technology: 'Azure DevOps (Azure version of tickets), JIRA, Confluence' },
+    ],
+    tcs: [
+      { area: 'Frontend', technology: 'React JS, Javascript' },
+      { area: 'Backend', technology: 'Express JS, Node JS' },
+      { area: 'Cloud', technology: 'AWS' },
+      { area: 'Cloud Services', technology: 'AWS Lambda, AWS S3, SNS, API Gateway services.' },
+      { area: 'Database', technology: 'AWS Aurora DB, PostgreSQL' },
+      { area: 'Version Control System', technology: 'Github' },
+      { area: 'CI/CD', technology: 'AWS Code Pipeline, Pull Request(PR) approach' },
+      { area: 'SDLC', technology: 'Local, Dev, UAT, Production' },
+      { area: 'Architecting', technology: 'Lucid Chart Diagram - Designing Database' },
+    ],
+    xebia: [
+      { area: 'Frontend', technology: 'React JS, Javascript' },
+      { area: 'Backend', technology: 'Node JS, Camunda BPMN Diagram, TAMM Workbench' },
+      { area: 'Store', technology: 'Redux Thunk' },
+      { area: 'Version Control System', technology: 'Gitlab' },
+      { area: 'CI/CD', technology: 'Used to deploy using Merge Requests (MR) approach' },
+      { area: 'SDLC', technology: 'Local, Stage, UAT, FUT, Go-Live, Production' },
+      { area: 'Tracking System', technology: 'JIRA, Confluence' },
+    ],
+  };
+  
